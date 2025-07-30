690bdfa3952c107c47a28cc3ea39000b048b454c
khanfaun <khangphan1612@gmail.com>
2025-07-30 14:22:15 +0700

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors({
-  origin: 'https://khanfaun-role-play-ai.netlify.app/',
+  origin: 'https://khanfaun-role-play-ai.netlify.app',
   methods: ['GET', 'POST', 'OPTIONS'],
   allowedHeaders: ['Content-Type'],
 }));
